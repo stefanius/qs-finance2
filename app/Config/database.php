41517bdd6f759f069d25b7b2dedcbb828a74e139
stefanius <sgrootveld@gmail.com>
2013-05-19 10:32:37 +0200

    Changed gitigonore

---
+++ app/Config/database.php
@@ -0,0 +1,24 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'datasource' => 'Database/Mysql',
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => '185.10.51.80',
+		'login' => 'dwivoqfb_aap',
+		'password' => 'grootjecms',
+		'database' => 'dev_qsfinance',
+	);
+
+/*
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'caketest',
+	);*/
+}
+?>
