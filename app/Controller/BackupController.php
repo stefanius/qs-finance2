ae2710ec145638b2b2abeb273e114ae60ceaaf72
stefanius <sgrootveld@gmail.com>
2013-06-09 22:38:22 +0200

    Added view for download mysql backups

---
+++ app/Controller/BackupController.php
@@ -0,0 +1,31 @@
+<?php
+App::uses('Folder', 'Utility');
+App::uses('File', 'Utility');
+
+class BackupController extends AppController {
+    
+    var $name = 'Backup';
+    var $uses = array();
+
+    function downloadmysql($filename=false){
+        $path=WWW_ROOT . DS . 'files' . DS . 'mysqlbackup' . DS;
+        if($filename===false){
+            $dir = new Folder($path);
+            $files = $dir->find('.*\.gz');
+            $this->set(compact('files'));
+        }else{
+            $this->viewClass = 'Media';
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            $basename = str_replace('.'.$ext, '', $filename );
+            $params = array(
+                'id'        => $filename,
+                'name'      => $basename,
+                'download'  => true,
+                'extension' => $ext,
+                'path'      => $path
+            );
+            $this->set($params);           
+        }
+    }    
+}
+
