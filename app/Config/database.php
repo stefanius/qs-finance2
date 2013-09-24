70267487375232f83d334bb679cb635248cd6d8b
stefanius <stefanius@asuslap>
2013-09-24 21:42:49 +0200

    Skelleton file for setting up database. This can be replaced on the live server, the settings are optimized for staging.

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,14 +1,15 @@
 <?php
 class DATABASE_CONFIG {
 
+	var $qsabbr = 'dev_';
 	var $default = array(
 		'datasource' => 'Database/Mysql',
 		'driver' => 'mysql',
 		'persistent' => false,
-		'host' => 'db.qsfinance.dev',
-		'login' => 'qsfinweb',
-		'password' => 'monkey88',
-		'database' => 'qsfin_dev',
+		'host' => 'db.mysql.qsfinance',
+		'login' => 'qs_'.$this->qsabbr.'user',
+		'password' => 'password',
+		'database' => 'qs_'.$this->qsabbr.'dev',
 	);
 
 /*
