068c142f6bde1e3919f69614baa408c4c0efa17f
Stefanius <sgrootveld@gmail.com>
2013-09-29 23:10:10 +0200

    Added basic Grootboeken as insert SQL.

--- sql/qsfinance.sql
+++ sql/qsfinance.sql
@@ -133,3 +133,56 @@ VALUES
 '2011-09-19 16:27:30',
 '2011-09-19 20:52:20'
 );
+
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e611700-131c-4b7b-85d0-14649d87f14d','0700','Voorraad','debet',0,'2011-09-02 17:48:48','2011-09-02 17:48:48',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4c01e3-47f8-457c-8e3d-17209d87f14d','0110','Bank - Lopende Rekening (rabo)','debet',0,'2011-08-17 18:01:07','2013-09-11 20:08:36',1);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e581ea5-6934-493c-bf38-14649d87f14d','0111','Bank - Lopende rekening (ing)','debet',0,'2011-08-26 22:31:01','2011-09-26 17:46:57',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e581f04-7efc-48ce-8d4b-14649d87f14d','0112','Bank - Spaarrekening','debet',0,'2011-08-26 22:32:36','2012-11-19 19:58:12',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4c018b-7288-49bd-ae3a-17209d87f14d','0076','Lening','credit',0,'2011-08-17 17:59:39','2011-08-17 17:59:39',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4c01b3-0ff8-4272-a5a1-17209d87f14d','0100','Kas','debet',0,'2011-08-17 18:00:19','2011-08-17 18:00:19',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4c01f2-6934-46db-891c-17209d87f14d','0130','debiteuren','debet',0,'2011-08-17 18:01:22','2011-08-17 18:01:22',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4c0266-7e44-4c36-b40d-17209d87f14d','0140','Crediteuren','credit',0,'2011-08-17 18:03:18','2011-08-17 18:03:18',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e581e4e-e3f8-4601-9810-14649d87f14d','9999','Kruispost: Voorschotten','credit',1,'2011-08-26 22:29:34','2011-08-26 22:29:34',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed10d-da5c-40d7-9cad-17209d87f14d','0420','Huisvestingskosten','credit',1,'2011-08-19 21:09:33','2011-08-19 21:09:33',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed172-9294-44c0-9373-17209d87f14d','0800','Inkoopwaarde verkopen','credit',1,'2011-08-19 21:11:14','2011-08-19 21:22:22',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed1c1-7338-480e-a396-17209d87f14d','0840','Opbrengst Verkopen','credit',1,'2011-08-19 21:12:33','2011-08-19 21:22:50',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed1d3-56ec-406c-8b17-17209d87f14d','0910','Kasverschillen','credit',1,'2011-08-19 21:12:51','2011-08-19 21:22:38',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed1e9-504c-4212-9798-17209d87f14d','0920','Voorraad Verschillen','credit',1,'2011-08-19 21:13:13','2011-08-19 21:13:13',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e4ed219-e974-4aba-a431-17209d87f14d','0950','Incidentele resultaten','credit',1,'2011-08-19 21:14:01','2011-08-19 21:14:01',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e80bc3f-fb18-4eab-9bf3-15189d87f14d','0424','Rekeningkosten','credit',1,'2011-09-26 17:54:07','2011-09-26 17:54:07',0);
+
+INSERT INTO `qs_dev_db`.`grootboeks` (`id`,`nummer`,`omschrijving`,`debetcredit`,`winstverlies`,`created`,`modified`,`liquide`) 
+VALUES ('1e80bc76-b5a4-408c-b016-15189d87f14d','0425','Interestkosten','credit',1,'2011-09-26 17:55:02','2011-09-26 17:55:02',0);
+
