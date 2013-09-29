c91edd9567c5d72da8ffbf99719a2935ee749e74
Stefanius <sgrootveld@gmail.com>
2013-09-29 23:10:40 +0200

    CakePHP unit test

---
+++ app/Test/Case/Controller/BankaccountsControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+App::uses('BankaccountsController', 'Controller');
+
+/**
+ * BankaccountsController Test Case
+ *
+ */
+class BankaccountsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.bankaccount',
+		'app.grootboek',
+		'app.bookyear',
+		'app.calculation'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+	}
+
+}
