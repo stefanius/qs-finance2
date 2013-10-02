7b8c855f1ed1c69a2a66a1087ca01cf4e5655993
Stefanius <sgrootveld@gmail.com>
2013-10-02 22:01:39 +0200

    Added Rabo-CSV support

--- app/Controller/Component/ImportRaboCsvComponent.php
+++ app/Controller/Component/ImportRaboCsvComponent.php
@@ -1,23 +1,75 @@
 <?php
 
-App::uses('Component', 'Controller', 'ImportComponent');
+App::uses('Component', 'ImportComponent');
 class ImportRaboCsvComponent extends ImportComponent {
         
     public function execute($filename = null, $source = null, $type = null){
-    	throw new NotImplementedException('RaboCSV import Not implemented yet');
+        $data = array();
+        $sourceinfo = array();
+        if(is_null($filename) || is_null($source) || is_null($type)){
+            throw new CakeException();
+        }
+            $this->Csv->setFirstLineAsHeader(true);
+            $this->Csv->setUseHeaderAsKey(false);
+            $csvdata =  $this->Csv->read($filename);
+            
+            foreach($csvdata as $key=>$datarow){
+                $data[$key]['boekdatum'] = $datarow[7];
+                $data[$key]['omschrijving'] = '';
+                
+                $data[$key]['omschrijving'] = $datarow[6] ;
+                for($i=10; $i<16;$i++){
+                	$data[$key]['omschrijving'] .= ' '. $datarow[$i] ;
+                }
+                $data[$key]['omschrijving'] = preg_replace('/\s+/', ' ', $data[$key]['omschrijving']) ;
+                
+                if(strlen($data[$key]['boekdatum']) == 8){
+                	$year = substr($data[$key]['boekdatum'], 0,4);
+                	$month = substr($data[$key]['boekdatum'], 4,2);
+                	$day = substr($data[$key]['boekdatum'], 6,2);
+                	$data[$key]['boekdatum'] = $year.'-'.$month.'-'.$day;
+                }else{
+                	throw new CakeException('Boekdatum RABO incorrect.');
+                }
+                
+                $sourceinfo['rekening'] = $datarow[2];
+                            
+                if($datarow[5] === 'D'){
+                    $data[$key]['credit'] = $datarow[4] ;
+                    $data[$key]['debet'] = 0;
+                }else{
+                    $data[$key]['debet'] =  $datarow[4] ;
+                    $data[$key]['credit'] = 0;                    
+                }
+            }
+        
+        $rtrn = array();
+        $rtrn['data']  = $data;
+        $rtrn['sourceinfo']  = $sourceinfo;
+        return $rtrn;
     } 
-
-
+   
 /*
-  0 => string 'Datum' (length=5)
-  1 => string 'Naam / Omschrijving' (length=19)
-  2 => string 'Rekening' (length=8)
-  3 => string 'Tegenrekening' (length=13)
-  4 => string 'Code' (length=4)
-  5 => string 'Af Bij' (length=6)
-  6 => string 'Bedrag (EUR)' (length=12)
-  7 => string 'MutatieSoort' (length=12)
-  8 => string 'Mededelingen' (length=12)
+
+0 => REKENINGNUMMER_REKENINGHOUDER Alfanumeriek 35 Eigen Rekeningnummer in IBAN formaat
+1 => MUNTSOORT Alfanumeriek 3 EUR
+2 => RENTEDATUM Numeriek 8 Formaat: EEJJMMDD
+3 => BY_AF_CODE Alfanumeriek 1 D of C (D=af; C=bij - let op! Anders dan je zou verwachten!)
+4 => BEDRAG Numeriek 14 2 decimalen. Decimalen worden weergegeven met een punt
+5 => TEGENREKENING Alfanumeriek 35 Tegenrekeningnummer
+6 => NAAR_NAAM Alfanumeriek 70 Naam van de tegenrekeninghouder
+7 => BOEKDATUM Numeriek 8 Formaat: EEJJMMDD
+8 => BOEKCODE Alfanumeriek 2 Type transactie
+9 => FILLER Alfanumeriek 6
+10 => OMSCHR1 Alfanumeriek 35
+11 => OMSCHR2 Alfanumeriek 35
+12 => OMSCHR3 Alfanumeriek 35
+13 => OMSCHR4 Alfanumeriek 35
+14 => OMSCHR5 Alfanumeriek 35
+15 => OMSCHR6 Alfanumeriek 35
+16 => END_TO_END_ID Alfanumeriek 35 SEPA Credit Transfer: Kenmerk opgegeven door de opdrachtgever
+17 => ID_TEGENREKENINGHOUDER Alfanumeriek 35 SEPA Credit Transfer: Identificatie van de tegenrekeninghouder
+18 => MANDAAT_ID Alfanumeriek 35 SEPA Direct Debet: Kenmerk machtiging 
  */
 }
 ?>
