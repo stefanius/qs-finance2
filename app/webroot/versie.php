47cc2b980c6110425a8164c72308db5ea0672d51
Stefanius <sgrootveld@gmail.com>
2013-10-04 21:48:43 +0200

    Deleted versie.php file.

--- app/webroot/versie.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Zet het SVN buildnummer & trunk/brach/tag in het systeem
-
-    $build = "0000";
-    $source = "UNSET";
-    Configure::write('Versie.build',$build);
-    Configure::write('Versie.source',$source);
