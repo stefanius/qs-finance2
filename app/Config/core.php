781e8f8f41da50b8b01fba54f3162d8b84b77539
Stefanius <sgrootveld@gmail.com>
2013-10-05 11:46:03 +0200

    Changed debug-settings. It is now read from the Apache VHost-settings instead of the Cake config-file

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,8 +32,20 @@
  *
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
+ * 
+ * Change level in Apache config config (VHost-file) by adding this line:
+ * SetEnv CAKEPHP_DEBUG 2
+ * 
+ * Where '2' can be replaced by the debug-setting 0,1 or 2.
  */
-    Configure::write('debug', 2);
+
+
+
+if (getenv('CAKEPHP_DEBUG')) {
+        Configure::write('debug', getenv('CAKEPHP_DEBUG'));
+} else {
+        Configure::write('debug', 0);
+}
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
