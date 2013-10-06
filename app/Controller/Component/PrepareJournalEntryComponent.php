46a033bd313430f20f464f2d15e2a218d5aee2c9
Stefanius <sgrootveld@gmail.com>
2013-10-06 13:54:18 +0200

    Added new Component for preparing new JournalEntrys (Calculations)

---
+++ app/Controller/Component/PrepareJournalEntryComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+App::uses('Component');
+class PrepareJournalEntryComponent extends Component
+{
+	public function prepareSingleTransaction(){
+		
+	}
+	
+	public function prepareBatchTransaction(){
+	
+	}
+	
+	public function generateHash(){
+	
+	}	
+}
