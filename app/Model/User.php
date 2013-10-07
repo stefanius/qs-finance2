b17baadf025ba04160f7dda90bda7d0ed8d8a361
Stefanius <sgrootveld@gmail.com>
2013-10-07 22:58:02 +0200

    Updated hashing for new version of Auth component

--- app/Model/User.php
+++ app/Model/User.php
@@ -64,4 +64,12 @@ class User extends AppModel
             return array('Group' => array('id' => $groupId));
         }
     }
+    
+    public function beforeSave($options = array()) {
+    	if (isset($this->data[$this->alias]['password'])) {
+    		$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+    	}
+    	return true;
+    }
+    
 }
