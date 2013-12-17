ce7be11960bd318505e7daa508cf795cd13689eb
Stefanius <sgrootveld@gmail.com>
2013-12-18 00:27:28 +0100

    Changed qs-finance deploy root

--- app/Config/app.rb
+++ app/Config/app.rb
@@ -1,6 +1,6 @@
 set :application, "qsfinance"
 set :repository,  "https://github.com/stefanius/qs-finance2.git"
-set :deploy_to, "/home/stefanius/sites/qsfinance"
+set :deploy_to, "/home/stefanius/sites/qsfinance2"
 set :branch, "master"
 set :user, "stefanius"
 set :use_sudo, false
