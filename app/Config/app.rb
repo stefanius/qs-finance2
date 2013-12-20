2588dac8cd83c2b6dbfb87859002e98814ea7f47
Stefanius <sgrootveld@gmail.com>
2013-12-20 23:34:14 +0100

    Improved deployment. Added an option to manualy adding a release branch

--- app/Config/app.rb
+++ app/Config/app.rb
@@ -2,7 +2,7 @@ set :application, "qsfinance2"
 set :stages, ["staging", "bv", "prive"]
 set :repository,  "https://github.com/stefanius/qs-finance2.git"
 set :deploy_to, defer { "/home/stefanius/sites/#{application}/#{stage}" }
-set :branch, "master"
+set :branch, fetch(:branch, "master")
 set :user, "stefanius"
 set :use_sudo, false
 set :upload_dirs, %w{import}
