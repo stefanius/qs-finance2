094ecb568f4df650b2dc1ed8abf955370f7fe4a9
Stefanius <sgrootveld@gmail.com>
2014-02-04 20:14:57 +0100

    Updated Capistrano

--- app/Config/app.rb
+++
@@ -1,9 +0,0 @@
-set :application, "qsfinance2"
-set :stages, ["staging", "bv", "prive"]
-set :repository,  "https://github.com/stefanius/qs-finance2.git"
-set :deploy_to, defer { "/home/stefanius/sites/#{application}/#{stage}" }
-set :branch, fetch(:branch, "master")
-set :user, "stefanius"
-set :use_sudo, false
-set :upload_dirs, %w{import}
-role :web, "fortstefanius.nl"
--- app/Config/deploy/prive.rb
+++
@@ -1,9 +0,0 @@
-set :stage, :prive
-set :application, "qsfinance2"
-set :repository,  "https://github.com/stefanius/qs-finance2.git"
-set :deploy_to,  "/home/stefanius/sites/qsfinance2/prive" 
-set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
-
-set :upload_dirs, %w{import}
-
-server "fortstefanius.nl", user: "stefanius", roles: %w{web}
